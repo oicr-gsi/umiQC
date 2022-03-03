74df3a22e8cf6c50944588d4f1a3a6390cec54fa
Murto Hilali <mhilali@oicr.on.ca>
2022-03-03 16:20:22 -0500

    Reverting calculate script

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -o nounset
-set -o errexit
-set -o pipefail
-
-#enter the workflow's final output directory ($1)
 cd $1
 
-#find all files, return their md5sums to std out
-find . -xtype f -exec md5sum {} +
+for file in *
+do 
+    echo $(md5sum $file) | awk '{print $1}'
+done
+
+ls | echo $(grep -c "tsv") "tsv"
+ls | echo $(grep -c "json") "json"
