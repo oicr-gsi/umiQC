aa9792bff33fa8b22c6e1f0437fb876d0e8bb8d9
Murto Hilali <murto.hilali@oicr.on.ca>
2022-03-07 16:02:10 -0500

    Modifying validator to account for metrics TSV

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd $1
 
-for file in *
+jsons=$(ls *.json)
+for file in $jsons
 do 
     echo $(md5sum $file) | awk '{print $1}'
 done
+
+echo $(awk '{$7=""}1' *.tsv | md5sum) | awk '{print $1}'
 
 ls | echo $(grep -c "tsv") "tsv"
 ls | echo $(grep -c "json") "json"
