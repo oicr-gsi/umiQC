94578d19dccd55b74c4b1ccf11f3b0fd03d13778
Murto Hilali <mhilali@oicr.on.ca>
2022-03-08 15:59:25 -0500

    Updating validator to sort md5sums

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -4,10 +4,13 @@ cd $1
 jsons=$(ls *.json)
 for file in $jsons
 do 
-    echo $(md5sum $file) | awk '{print $1}'
+    md5sums+=$(md5sum $file | awk '{print $1" "}')
 done
 
-echo $(awk '{$7=""}1' *.tsv | md5sum) | awk '{print $1}'
+md5sums+=$(echo $(awk '{$7=$8=$9=""}1' *.tsv | md5sum) | awk '{print $1}')
+
+printf '%s\n' ${md5sums[*]} | sort
+
 
 ls | echo $(grep -c "tsv") "tsv"
 ls | echo $(grep -c "json") "json"
