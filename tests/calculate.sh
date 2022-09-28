6fadeca349f683cebab60c9e91f9e6cd7554e35a
Gavin Peng <gpeng@oicr.on.ca>
2022-09-28 14:46:37 -0400

    change test file

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 cd $1
 
-#find all json  files, return their md5sums to std out, list all file types
-find . -name "*.json" -xtype f -exec sh -c "cat {} | md5sum" \;
+find . -name "output*" -xtype f -exec sh -c "cat {} | md5sum" \;
 ls | sed 's/.*\.//' | sort | uniq -c
 
